ee490c8ee1e9580efcd055b41cbea6d7675f2336
Rafal <rafaljaneczek@o2.pl>
2019-12-21 21:06:13 +0100

    initial commit

---
+++ src/test/java/GoogleSearchTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class GoogleSearchTest {
+    private WebDriver driver;
+
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C://Users//Rafał//Documents//chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+    }
+
+    @Test
+    public void testGoogleSearch() {
+        WebElement element = driver.findElement(By.name("q"));
+        element.clear();
+
+        element.sendKeys("Selenium testing tools cookbok");
+
+        element.submit();
+
+        new WebDriverWait(driver, 10).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("selenium testing tools cookbook");
+            }
+        });
+        assertEquals("Selenium testing tools cookbook - Google Search" , driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        driver.quit();
+    }
+
+}
